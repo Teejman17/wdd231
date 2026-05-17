5cb4c53457b7b92d66f034a59e2b7bfc2c552a46
Teejman17 <teejman17@gmail.com>
2026-05-16 22:30:47 -0600

    update W02

--- week02/scripts/prophets.js
+++ week02/scripts/prophets.js
@@ -2,13 +2,6 @@ const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.
 
 const cards = document.querySelector('#cards');
 
-async function getProphetData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    // console.table(data.prophets);
-    displayProphets(data.prophets);
-}
-
 const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
         let card = document.createElement('section');
@@ -26,7 +19,14 @@ const displayProphets = (prophets) => {
 
         cards.appendChild(card);
     });
+};
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
+}
 
 getProphetData();
 
-}
