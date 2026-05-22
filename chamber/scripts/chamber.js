2dfe6477dca9cf74ccb4079c0c9f59e970f9c1c6
Teejman17 <teejman17@gmail.com>
2026-05-21 22:20:29 -0700

    Progress updates

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -6,13 +6,13 @@ navButton.addEventListener('click', () => {
     navLinks.classList.toggle('show');
 });
 
-const url = "data/member.json";
+const url = "../data/members.json";
 const cards = document.querySelector("#membersSection");
 
 const displayMembers = (members) => {
     members.forEach((member) => {
         let card = document.createElement('section');
-        let logo = document.createElement('img');
+        let img = document.createElement('img');
         let name = document.createElement('h2');
         let address = document.createElement('p');
         let phone = document.createElement('p');
@@ -20,10 +20,37 @@ const displayMembers = (members) => {
         let level = document.createElement('p');
         let slogan = document.createElement('p');
         let established = document.createElement('p');
-        companyName.textContent = member.companyName;
-    }
-    )
-}
+        name.textContent = member.companyName;
+        address.textContent = member.companyAddress;
+        phone.textContent = `Phone: ${member.phoneNumber}`;
+        slogan.textContent = `"${member.slogan}"`;
+        established.textContent = member.establishedYear;
+
+        website.setAttribute('href', member.website);
+        img.setAttribute('src', `images/${member.img}`);
+        img.setAttribute('alt', `member image`);
+        img.setAttribute('loading', 'lazy');
+        img.setAttribute('width', '200');
+        img.setAttribute('height', '150');
+
+        let membershipLevel = 'member';
+        if (member.membership == 2) membershipLevel = 'Silver Member';
+        if (member.membership == 3) membershipLevel = 'Gold Member';
+        level.textContent = `Level: ${membershipLevel}`;
+
+        card.appendChild(img);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(slogan);
+        card.appendChild(website);
+        card.appendChild(level);
+        card.appendChild(established);
+
+        cards.appendChild(card)
+
+    });
+};
 
 async function getMemberData() {
     const response = await fetch(url);
@@ -31,7 +58,27 @@ async function getMemberData() {
     console.table(data.members);
     displayMembers(data.members);
     
-};
+}
+
+getMemberData();
+
+const gridButton = document.querySelector('#gridButton');
+const listButton = document.querySelector('#listButton');
+const displayMenu = document.querySelector('#memberSection');
+
+gridButton.addEventListener('click', () => {
+    displayMenu.classList.add('grid');
+    displayMenu.classList.remove('list');
+    gridButton.classList.add('active');
+    listButton.classList.remove('active');
+});
+
+listButton.addEventListener('click', () => {
+    displayMenu.classList.add('list');
+    displayMenu.classList.remove('gird');
+    listButton.classList.add('active');
+    gridButton.classList.remove('active');
+});
 
 const year = document.getElementById('currentYear');
 const currentYear = new Date().getFullYear();
