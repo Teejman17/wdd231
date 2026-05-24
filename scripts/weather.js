fdfcd87014a78bd2685f068f058f4a69a768ca23
Teejman17 <teejman17@gmail.com>
2026-05-23 18:45:11 -0700

    Learning activities progress

---
+++ scripts/weather.js
@@ -0,0 +1,21 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=metric&appid=776cef31d554a322795d49a08404ba59';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+        } else {
+            throw Error(await response.text());
+        }
+      } catch (error) {
+            console.log(error);
+        }
+}
+
+apiFetch();
