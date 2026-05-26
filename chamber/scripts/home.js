2bc1ebf54227c890f899570fa7dba3000765664b
Teejman17 <teejman17@gmail.com>
2026-05-26 08:25:10 -0600

    Chamber Home final updates

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -87,3 +87,90 @@ function displayForecast(forecastData) {
 }
 
 apiFetch();
+
+// SPOTLIGHT MEMBERS SECTION
+ 
+const membersURL = '../data/members.json';
+
+async function getMembers() {
+    try {
+        const response = await fetch(membersURL);
+        if (response.ok) {
+            const data = await response.json();
+            // console.log(data);
+
+            displaySpotlight(data.members);
+        } else {
+            throw Error(await response.text());
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displaySpotlight(members) {
+    const spotlightMembers = members.filter(member => member.membership === 2 || member.membership === 3);
+
+
+    const spotlightCards = document.querySelector('#spotlightCards');
+    const randomMembers = spotlightMembers.sort(() => 0.5 - Math.random());
+    const chosenMembers = randomMembers.slice(0, 3);
+
+    spotlightCards.innerHTML = '';
+
+    chosenMembers.forEach(member => {
+        let card = document.createElement('section');
+        card.className = 'memberCard';
+        let img = document.createElement('img');
+        let name = document.createElement('h2');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let website = document.createElement('a');
+        let level = document.createElement('p');
+        let slogan = document.createElement('p');
+        let established = document.createElement('p');
+
+        name.textContent = member.companyName;
+        address.textContent = member.companyAddress;
+        phone.textContent = `Phone: ${member.phoneNumber}`;
+        slogan.textContent = `"${member.slogan}"`;
+        established.textContent = `Since: ${member.establishedYear}`;
+        website.href = member.website;
+        website.textContent = `Visit ${member.companyName}'s website`;
+
+        img.setAttribute('src', `images/${member.img}`);
+        img.setAttribute('alt', `member image`);
+        img.setAttribute('loading', 'lazy');
+        img.setAttribute('width', '200');
+        img.setAttribute('height', '150');
+
+        let membershipLevel = 'member';
+        if (member.membership == 2) membershipLevel = 'Silver Member';
+        if (member.membership == 3) membershipLevel = 'Gold Member';
+        level.textContent = `Level: ${membershipLevel}`;
+        
+        card.appendChild(name);
+        card.appendChild(img);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(slogan);
+        card.appendChild(website);
+        card.appendChild(level);
+        card.appendChild(established);
+
+        spotlightCards.appendChild(card)
+
+    });
+}
+
+getMembers();
+
+
+const year = document.getElementById('currentYear');
+const currentYear = new Date().getFullYear();
+year.textContent = currentYear;
+
+
+const lastModified = document.getElementById('lastModified');
+lastModified.textContent = `Last Modified ${document.lastModified}`;
