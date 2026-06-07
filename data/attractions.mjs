ef9d5c8c427bc55c3caff49ff91cb13fa124f713
Teejman17 <teejman17@gmail.com>
2026-06-07 09:24:19 -0600

    Discover updates

---
+++ data/attractions.mjs
@@ -0,0 +1,52 @@
+{
+    "attractions"; [
+        {
+            "name": "Dixie Rock - Pioneer Park",
+            "address": "375 Red Hills Pkwy, St. George, UT 84770",
+            "description": "Point of interest to see the St. George skyline and take in the true red/orange natural colors the city has to offer."
+            
+        },
+        {
+            "name": "Tuacahn Amphitheatre",
+            "address": "1100 Tuacahn Dr, Ivins, UT 84738",
+            "description": "Outdoor amphitheatre experience to catch musicals, concerts, and more."
+
+        },
+        {
+            "name": "Sand Hollow Reservior",
+            "address": " 3351 South Sand Hollow Road, Hurricane, UT 84737",
+            "description": "Beautiful place to cool off surrounded by the natural landscape southern Utah has to offer about a half hour drive from St. George, well known for cliff jumping into the refreshing water."
+
+        },
+        {
+            "name": "Zion National Park",
+            "address": "1 Zion Park Blvd, Springdale, UT 84767",
+            "description": "One of Utah's greatest well-known national parks with opportunites to hike breathtaking trails that lead you to incredible views."
+
+        },
+        {
+            "name": "Town Square Park",
+            "address": "50 S Main St, St. George, UT 84770",
+            "description": "This park located downtown is at the central of St. George. Open green areas, a splashpad, and a small river can be found to enjoy your time in the sun."
+
+        },
+        {
+            "name": "Thunder Junction Park",
+            "address": "1851 S Dixie Dr, St. George, UT 84770",
+            "description": "This park is a typical family favorite. Designed with a dinosaur theme you can find dinosaurs all around, and even a volcano. You can also take a train ride around the park, or cool off at the small splash pad that includes waterfalls."
+
+        },
+        {
+            "name": "Washington Wheels Park",
+            "address": "3730 Rolling Rock Dr, Washington, UT 84780",
+            "description": "One of St. George's newest parks! Offers a decent sized skate park for all levels of riders, a splash pad to cool off on warm days, and an enourmous playground to spend hours of fun."
+
+        },
+        {
+            "name": "Dixie Red Hills Golf Course",
+            "address": "645 W 1250 N, St. George, UT 84770",
+            "description": "This golf course is surrounded by the red hills that are typical in St. George. A 9-hole course that gives you that true St. George feel."
+
+        }
+    ]
+}
