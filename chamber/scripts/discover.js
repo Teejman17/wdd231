aaea54c66efe5467213f69383090da19d842d184
Teejman17 <teejman17@gmail.com>
2026-06-08 18:20:15 -0600

    Discover updates

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -47,6 +47,30 @@ function displayItems(attractions) {
 
 displayItems(attractions);
 
+const visitMessage = document.querySelector('#visitDate');
+const currentVisitDate = Date.now();
+const lastVisit = localStorage.getItem('last-visit-date');
+
+if (!lastVisit) {
+    visitMessage.textContent = 'Welcome! Let us know if you have any questions.';
+} else {
+    const timeDifference = currentVisitDate - parseInt(lastVisit);
+    const msDays = 86400000;
+    const daysBetween = Math.floor(timeDifference / msDays);
+
+    if (timeDifference < msDays) {
+        visitMessage.textContent = 'Back so Soon! Awesome';
+    } else {
+        if (daysBetween === 1) {
+            visitMessage.textContent = 'You last visited 1 day ago';
+        } else {
+            visitMessage.textContent = `You last visited ${daysBetween} days ago`;
+        }
+
+    }
+}
+
+localStorage.setItem('last-visit-date', currentVisitDate);
 
 const year = document.getElementById('currentYear');
 const currentYear = new Date().getFullYear();
