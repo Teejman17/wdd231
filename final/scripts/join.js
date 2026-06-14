5b63ea24acf284033336eb4855d5c767cb81895a
Teejman17 <teejman17@gmail.com>
2026-06-14 14:14:54 -0600

    Contact start

---
+++ final/scripts/join.js
@@ -0,0 +1,33 @@
+const navButton = document.querySelector('#hamButton');
+const navLinks = document.querySelector('#navBar');
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navLinks.classList.toggle('show');
+});
+
+
+const dialog = document.querySelector('.modal dialog');
+const openButton = document.querySelector('.modalOpen');
+const closeButton = document.querySelector('.modalClose');
+
+openButton.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+closeButton.addEventListener('click', () => {
+    dialog.closest();
+});
+
+
+const timestamp = document.querySelector('#timestamp');
+if (timestamp) {
+    timestamp.value = new Date().toLocaleString();
+}
+
+const year = document.getElementById('currentYear');
+const currentYear = new Date().getFullYear();
+year.textContent = currentYear;
+
+const lastModified = document.getElementById('lastModified');
+lastModified.textContent = `Last Modified ${document.lastModified}`;
